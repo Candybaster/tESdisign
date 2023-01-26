97d327c94f613d489bff36be51c1adbd6d366fd0
Candybaster <116972274+Candybaster@users.noreply.github.com>
2023-01-26 23:49:23 +0300

    Add files via upload

---
+++ tESdisign/src/index.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", () => {});
+// window.addEventListener("scroll", ($event) => {
+//   const intro = document.querySelector(".intro").getBoundingClientRect();
+//   let pageYOffset = window.pageYOffset + document.documentElement.clientHeight;
+//   const title = document.querySelector(".intro__content__text");
+
+//   if(intro.top < -100 ){
+//     title.style.opacity = 0;
+//   } else {
+//     title.style.opacity = 1;
+//   }
+
+//   if(intro.bottom>0) {
+//     title.style.transform = `translateY(${intro.top}px)`;
+//   }
+//   console.log(window.pageYOffset);
+// });
